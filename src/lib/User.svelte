dc3ec34eab0bf992f6330df4d99940eac52c2bb3
Muhammad Islam <muhammad.cse36@gmail.com>
2022-11-25 16:20:50 +0100

    initial commit

---
+++ src/lib/User.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+
+    export let user;
+    export let i;
+    const dispatch = createEventDispatcher();
+    
+    const remove = () => {
+        dispatch('remove', user.id)
+    }
+</script>
+
+
+
+<div class="flex m-5 bg-black border p-2 
+rounded-lg shadow-md hover:shadow-lg 
+cursor-pointer items-center relative">
+    <button
+    on:click={remove}
+    class="absolute right-0 top-0 -mt-3 rounded-full w-5 h-5 text-center 
+    bg-red-300 text-white text-sm hover:bg-red-700">X</button>
+    <p class="px-10">{i + 1}</p>
+    <img class="rounded-full w-20 h-14 bg-white" src={user.icon} alt={user.name}>
+    <p class="mx-4 w-25">{user.name}</p>
+    <p class="mx-4 w-25">{user.email}</p>
+</div>
---
+++ src/lib/Users.svelte
@@ -0,0 +1,57 @@
+<script>
+  import user1 from '../assets/images/vite.svg';
+  import user2 from '../assets/images/group.png';
+  import user3 from '../assets/images/man.png';
+  import User from './User.svelte';
+  import FilterUser from './FilterUser.svelte';
+  
+  let users = [
+    {
+    id: 1,
+    name : "John doe",
+    icon : user1,
+    email: "john.doe@example.com",
+    active: true,
+  },
+  {
+    id: 2,
+    name : "Muhammad doe",
+    icon : user2,
+    email: "Muhammad.doe@example.com",
+    active: true,
+  },
+  {
+    id: 3,
+    name : "Hacker doe",
+    icon : user3,
+    email: "Hacker.doe@example.com",
+    active: false,
+  }
+];
+
+// reactivly update the variable
+$: filteredUser = users;
+
+const filter = (value) => {
+  if(value.detail === 'null'){
+      return filteredUser = users;
+  }
+  return filteredUser = users.filter((user)=> user.active === JSON.parse(value.detail.toLowerCase()))
+  
+}
+
+const remove = ({detail}) => {
+  users = users.filter((user) => user.id !== detail)
+}
+    
+</script>
+ <FilterUser on:filter={filter}/>
+<div>
+  <h1 class="text-2xl text-center mt-10">List of all users</h1>
+  {#each filteredUser as user, i (user.id)}
+  <User on:remove={remove} {user} i={i}/>
+  {:else}
+  <p>No User Found!!!</p>
+  {/each}
+</div>
+    
